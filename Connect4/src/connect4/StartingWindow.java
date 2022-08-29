80b7b3b6bd199bb12bc141fa42b4b75fef68561d
matti <mattia-giordano@hotmail.it>
2022-08-29 13:11:01 +0200

    updated with images

--- Connect4/src/connect4/StartingWindow.java
+++ Connect4/src/connect4/StartingWindow.java
@@ -6,10 +6,19 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.Font;
+
+
 
 public class StartingWindow {
 
 	private JFrame frmConnect;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -39,15 +48,42 @@ public class StartingWindow {
 	 */
 	private void initialize() {
 		frmConnect = new JFrame();
+		frmConnect.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\matti\\git\\Connect4\\Connect4\\img\\hggfhhgfgfh.jpg"));
 		frmConnect.setTitle("Connect4");
-		frmConnect.setBounds(100, 100, 629, 580);
+		frmConnect.setBounds(100, 100, 615, 508);
 		frmConnect.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmConnect.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("");
-		Image img = new ImageIcon(this.getClass().getResource("/Connect4/img/38753106.jpg")).getImage();
-		lblNewLabel.setIcon(new ImageIcon(img));
-		lblNewLabel.setBounds(0, 0, 613, 541);
+		JButton btnNewButton = new JButton("New Game");
+		btnNewButton.setBounds(238, 413, 122, 45);
+		frmConnect.getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(263, 196, 139, 20);
+		frmConnect.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(263, 239, 139, 20);
+		frmConnect.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Player 1:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1.setBackground(new Color(255, 0, 255));
+		lblNewLabel_1.setForeground(new Color(0, 0, 255));
+		lblNewLabel_1.setBounds(194, 202, 59, 14);
+		frmConnect.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Player 2:");
+		lblNewLabel_1_1.setForeground(new Color(0, 255, 0));
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1_1.setBounds(194, 241, 59, 14);
+		frmConnect.getContentPane().add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\matti\\git\\Connect4\\Connect4\\img\\hggfhhgfgfh.jpg"));
+		lblNewLabel.setBounds(0, 0, 599, 469);
 		frmConnect.getContentPane().add(lblNewLabel);
 	}
 }
